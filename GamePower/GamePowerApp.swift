a1bb0bf8e46cbccc5898e5f196480a2dbf3ffbfc
Amr Fawaz <amr.abdelhakim.fawaz+75@gmail.com>
2024-05-13 19:36:06 +0300

    Initial Commit

---
+++ GamePower/GamePowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePowerApp.swift
+//  GamePower
+//
+//  Created by AmrFawaz on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GamePowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
